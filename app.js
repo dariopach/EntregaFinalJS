f5d81afa1f1987fa6bde8eaa8f7ed14fb13ba14c
dariopach <dariopach@hotmail.com>
2023-05-05 18:55:16 -0300

    Primera version

--- app.js
+++ app.js
@@ -175,3 +175,22 @@ document.getElementById("btnLimpiar").addEventListener("click", function() {
     let ulStock = document.getElementById('ulStock');
     ulStock.innerHTML = '';
 });
+
+
+//Imagen ramdom
+
+const numItemsToGenerate = 1; 
+
+function renderItem(){
+  fetch(`https://source.unsplash.com/1600x900/?beach`).then((response)=> {   
+    let item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `
+      <img class="beach-image" src="${response.url}" alt="beach image"/>
+    `     
+    document.body.appendChild(item);
+  }) 
+}
+for(let i=0;i<numItemsToGenerate;i++){
+  renderItem();
+}
